38be83f12a5de06f27063a4e087f6718f7502485
AReum <whdkfma0507@gmail.com>
2020-09-08 10:19:46 +0900

    0908수업

--- q2_1.py
+++ q2_1.py
@@ -1,8 +1,8 @@
 #20단
 
 for i in range(2, 20):
-    print('['i', "단"]')
+    print('[', i, '단]')
     for j in range(1, 10):
-        print(i*j, end= " ")
+        print('%dx%d=%d'%(i,j,i*j))
     print('')
 
