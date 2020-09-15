01a081e41f95c96d747b68e8ab9a947fc5396d4d
AReum <whdkfma0507@gmail.com>
2020-09-15 10:46:04 +0900

    도서관리시스템

--- book.py
+++ book.py
@@ -7,14 +7,15 @@
 #1을 집어 넣으면 종료가 되고 2를 집어 넣으면 선택 3을 집어 넣으면 책 등록하기
 # 다른숫자 넣으면 다시 넣으세요
 
-
+'''
 f = open("도서관리시스템.txt", 'w',encoding='utf-8')
-data = ['ㄱ: 1, 2, 3, 4\n', 'ㄴ: 5, 6, 7, 8\n', 'ㄷ: 9, 10\n', 'ㄹ: 11, 12']
+data = ['ㄱ: 1, 2, 3, 4\n', 'ㄴ: 5, 6, 7, 8\n', 'ㄷ: 9, 10\n', 'ㄹ: 11, 12\n']
 for i in data:
     f.write(i)
 f.close()
 print(data)
 #메모장에 값 넣기
+'''
 
 print('-'*30)
 x = '\n1. 종료\n 2. 선택\n 3. 등록'
@@ -30,21 +31,37 @@ while True:
     elif put == '2':
         name = input("책 이름을 입력하세요: ")
         f = open("도서관리시스템.txt", 'r', encoding='utf-8')
-        list = f.readline()
-        if name in list:
-            print("책이름: ", list [0])
-            print("코드: ", list [3:])
-            f.close()
+        lst = f.read()
+        #print(type(list(lst)))
+        #print(list(lst))
+        lst = lst.split('\n')
+
+
+        name2 =''
+        code2 = ''
+
+        for x in lst:
+            for i, y in enumerate(x):
+                if y == ':':
+                    name2 = x[:i]
+                    code2 = x[i+1:]
+                    break
+
+            if name == name2:
+                print("책이름: ", name2)
+                print("코드: ", code2)
+                break
         else:
             print("책이 없습니다. 등록하시오.")
+        f.close()
 
     elif put == '3':
         name = input("책 이름을 입력하세요: ")
-        code = int(input("코드를 입력하세요: "))
-        f = open("도서관리시스템.txt", 'r', encoding='utf-8')
-        f.write('new')
+        code = input("코드를 입력하세요: ")#'1234' -> int() 1234
+        f = open("도서관리시스템.txt", 'a', encoding='utf-8')
+        f.write( name+':'+code+'\n' )
         f.close()
         print('-'*30)
         print("책이 입력 되었습니다.")
-        print(':', code )
+        print(':', code)
         print('-' * 30)
